2a287f9bd679f114a6b1a140e7ed993490b80048
nikita64643 <42143227+nikita64643@users.noreply.github.com>
2020-12-19 00:56:35 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string leftop = ""; // Левый операнд
+        string operation = ""; // Знак операции
+        string rightop = ""; // Правый операнд
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Добавляем обработчик для всех кнопок на гриде
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки
+            string s = (string)((Button)e.OriginalSource).Content;
+            // Добавляем его в текстовое поле
+            textBlock.Text += s;
+            int num;
+            // Пытаемся преобразовать его в число
+            bool result = Int32.TryParse(s, out num);
+            // Если текст - это число
+            if (result == true)
+            {
+                // Если операция не задана
+                if (operation == "")
+                {
+                    // Добавляем к левому операнду
+                    leftop += s;
+                }
+                else
+                {
+                    rightop += s;
+                }
+            } else {
+
+                if (s == "=") {
+                    Update_RightOp();
+                    textBlock.Text += rightop;
+                    operation = "";
+                } else if (s == "CLEAR") {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    textBlock.Text = "";
+                } else {
+
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+        private void Update_RightOp()
+        {
+            int num1 = Int32.Parse(leftop);
+            int num2 = Int32.Parse(rightop);
+
+            switch (operation)
+            {
+                case "+":
+                    rightop = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightop = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightop = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightop = (num1 / num2).ToString();
+                    break;
+            }
+        }
+    }
+}
